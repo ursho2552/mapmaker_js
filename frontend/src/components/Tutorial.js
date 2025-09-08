a5827467a9b328ffc1e6c82c458647d3fb257235
rkamysheva <rada.kamysheva@inf.ethz.ch>
2025-09-08 13:04:31 +0200

    tutorial cleanup

--- frontend/src/components/Tutorial.js
+++ frontend/src/components/Tutorial.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import InfoModal from "./InfoModal";
 import TutorialTooltip from "./TutorialTooltip";
+import { tooltips } from "../constants.js"
 
 const Tutorial = ({ start, onFinish, panel1Year, setTutorialStep }) => {
     const [step, setStep] = useState(0);
@@ -14,51 +15,6 @@ const Tutorial = ({ start, onFinish, panel1Year, setTutorialStep }) => {
     }, [panel1Year, step, setTutorialStep]);
 
     if (!start) return null;
-
-    // Tutorial steps
-    const tooltips = [
-        null, // Step 0: Welcome modal
-        { // Step 1
-            text: "You can compare different scenarios, models, and datasets side by side.",
-            top: "30%",
-            left: "50%",
-        },
-        { // Step 2
-            text: "Each data panel allows you to switch between a flat map and a 3D globe for convenience.",
-            top: "30%",
-            left: "25%",
-        },
-        { // Step 3
-            text: "Use the year slider to visualize changes over time.",
-            top: "35%",
-            left: "16%",
-        },
-        { // Step 4
-            text: "Each panel has a corresponding control panel, where you can select the scenario, model, and other parameters to display.",
-            top: "60%",
-            left: "50%",
-        },
-        { // Step 5
-            text: "Use locks to sync or separate panels. By default, the left and right data panels are synchronized, meaning the year, scenario, and model are linked. You can unlock these parameters individually if you want to compare different settings.",
-            top: "65%",
-            left: "50%",
-        },
-        { // Step 6
-            text: "For any parameter, click on the info icons to learn more about its meaning and source.",
-            top: "60%",
-            left: "40%",
-        },
-        { // Step 7
-            text: "You can select any point on the map or globe to explore how parameters evolve over time.",
-            top: "75%",
-            left: "17%",
-        },
-        { // Step 8
-            text: "Observe the time series for the selected point.",
-            top: "43%",
-            left: "50%",
-        }
-    ];
 
     return (
         <Box>
@@ -99,7 +55,7 @@ const Tutorial = ({ start, onFinish, panel1Year, setTutorialStep }) => {
                 </>
             )}
 
-            {/* Step 4 - Completion */}
+            {/* Step 9 - Completion */}
             <InfoModal
                 open={step === 9}
                 onClose={() => {
